517cd3cfe9d06d987a890c60a3a18eefb39e965e
cieclarke <chris@cieclarke.com>
2021-02-19 16:06:37 +0000

    cors

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 const port = 3000;
 const app = express();
 
 app.use(bodyParser.json());
+app.use(cors());
 
 app.get('/', (req, res) => {
     res.type("json");
